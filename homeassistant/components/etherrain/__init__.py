d3c1b3e6cdf0330187e7994012bf6ffd95268cd8
Herb Peyerl <hpeyerl+gitlab@beer.org>
2019-03-29 11:57:41 +0000

    update to work with 0.90

--- homeassistant/components/etherrain/__init__.py
+++ homeassistant/components/etherrain/__init__.py
@@ -8,19 +8,21 @@ https://home-assistant.io/components/etherrain/
 """
 import logging
 
-import etherrain
 import voluptuous as vol
 
 from homeassistant.const import (
     CONF_HOST, CONF_PASSWORD, CONF_USERNAME)
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['etherrain==0.3']
+REQUIREMENTS = ['etherrain==0.6']
 
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_TIMEOUT = 10
 DOMAIN = 'etherrain'
+
+LOGIN=False
+ER=None
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -33,20 +35,28 @@ CONFIG_SCHEMA = vol.Schema({
 
 def setup(hass, config):
     """Set up the Etherrain component."""
+    import etherrain
 
     conf = config[DOMAIN]
+
+    global LOGIN
+    global ER
 
     hostname = conf[CONF_HOST]
     username = conf.get(CONF_USERNAME, None)
     password = conf.get(CONF_PASSWORD, None)
 
     hass.data[DOMAIN] = {}
-    hass.data[DOMAIN]['er'] = EtherRainAPI(hostname, username, password, timeout=DEFAULT_TIMEOUT)
+    ER = EtherRainAPI(hostname, username, password, timeout=DEFAULT_TIMEOUT)
 
-    return hass.data[DOMAIN]['er'].logged_in
+    LOGIN=ER.logged_in
+
+    return LOGIN
 
 class EtherRainAPI(object):
+
     def __init__(self, hostname, username, password, timeout):
+        import etherrain
         self.er = etherrain.EtherRain(hostname, username, password, timeout=DEFAULT_TIMEOUT)
         self.logged_in = self.er.login()
         self.update()
---
+++ homeassistant/components/etherrain/switch.py
@@ -0,0 +1,65 @@
+import logging
+
+import voluptuous as vol
+
+from homeassistant.components import etherrain
+from homeassistant.components.switch import (SwitchDevice, PLATFORM_SCHEMA)
+from homeassistant.const import CONF_NAME
+import homeassistant.helpers.config_validation as cv
+
+CONF_VALVES = 'valves'
+CONF_DURATION = 'duration'
+
+VALVE_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME): cv.string,
+    vol.Required(CONF_DURATION):cv.positive_int,
+})
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_VALVES, default={}):
+        vol.Schema({cv.positive_int: VALVE_SCHEMA}),
+})
+
+DEPENDENCIES = ['etherrain']
+
+_LOGGER = logging.getLogger(__name__)
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """ Setup the Etherrain platform."""
+    # Verify that we're logged into an Etherrain device
+
+    if etherrain.LOGIN is False:
+        _LOGGER.error("No active connection to an Etherrain device")
+        return False
+
+    valves = config.get(CONF_VALVES)
+
+    switches = []
+    for valvenum,duration in valves.items():
+        switches.append(EtherRainSwitch(valvenum,duration))
+
+    add_entities(switches)
+
+class EtherRainSwitch(SwitchDevice):
+
+    def __init__(self, valve, options):
+        self._valve = valve
+        self._duration = options.get(CONF_DURATION)
+        self._name = options.get(CONF_NAME)
+
+        self.on_handler = etherrain.ER.water_on
+        self.off_handler = etherrain.ER.water_off
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def is_on(self):
+        return etherrain.ER.get_state(self._valve)
+
+    def turn_on(self, **kwargs):
+        self.on_handler(self._valve, self._duration)
+
+    def turn_off(self, **kwargs):
+        self.off_handler(self._valve)
